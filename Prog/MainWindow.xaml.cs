cdf35b96b28489ee067a30bbcbb9e0679df09244
Nicojasy <dimataranapa12@gmail.com>
2020-08-05 23:59:02 +0300

    added error

--- Prog/MainWindow.xaml.cs
+++ Prog/MainWindow.xaml.cs
@@ -1,23 +1,11 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
 using System.Diagnostics;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Prog
 {
@@ -121,6 +109,7 @@ namespace Prog
             catch (Exception exc)
             {
                 Debug.WriteLine(exc, "Warning");
+                MessageBox.Show("Error!");
                 return;
             }
         }
@@ -198,6 +187,7 @@ namespace Prog
                 catch (Exception e)
                 {
                     Debug.WriteLine(e, "Warning");
+                    MessageBox.Show("Error!");
                     return null;
                 }
             }
